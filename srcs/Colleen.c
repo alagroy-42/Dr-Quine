bb688e83d8eb20ceb2bc69505208202e4462d8e2
alagroy- <alagroy-@student.42.fr>
2021-03-11 19:52:10 +0100

    adding Colleen in C

--- srcs/Colleen.c
+++ srcs/Colleen.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-
 /*
 	First Comment
 */
-
-void	disp()
+void display(char *s, char *disp, char *main)
 {
-	
+	printf(s, 10, 10, 9, 10, 10);
+	printf(disp, 10, 10, 9, 10, 9, 10, 9, 10, 10);
+	printf(main, 10, 10, 9, 34, s, 34, 10, 9, 34, disp, 34, 10, 9, 34, main, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);
 }
-
-int		main()
+int main()
 {
+	char *s="#include <stdio.h>%c/*%c%cFirst Comment%c*/%c";
+	char *disp="void display(char *s, char *disp, char *main)%c{%c%cprintf(s, 10, 10, 9, 10, 10);%c%cprintf(disp, 10, 10, 9, 10, 9, 10, 9, 10, 10);%c%cprintf(main, 10, 10, 9, 34, s, 34, 10, 9, 34, disp, 34, 10, 9, 34, main, 34, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c}%c";
+	char *main="int main()%c{%c%cchar *s=%c%s%c;%c%cchar *disp=%c%s%c;%c%cchar *main=%c%s%c;%c%c/*%c%c%cSecond Comment%c%c*/%c%cdisplay(s, disp, main);%c}%c";
 	/*
 		Second Comment
 	*/
+	display(s, disp, main);
 }
